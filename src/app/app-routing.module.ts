96144dd09b79bdb1307555809e9740e98921b0f1
jackthomsonn <hello@jackthomson.co.uk>
2021-08-06 23:55:39 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { PrivacyComponent } from './privacy/privacy.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'privacy',
     component: PrivacyComponent,
+  },
+  {
+    path: 'terms-and-conditions',
+    component: TermsAndConditionsComponent,
   },
 ];
 
